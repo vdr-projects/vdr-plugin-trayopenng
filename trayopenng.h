d915ac96ce0c2e67bfca31dad947cc1dbd9824c5
Ulrich Eckhardt <uli@uli-eckhardt.de>
2010-05-13 17:07:38 +0200

    Fix compile Makefile for vdr 1.6.0

--- trayopenng.h
+++ trayopenng.h
@@ -15,7 +15,7 @@
 #include <vdr/status.h>
 #include "setup.h"
 
-static const char *VERSION        = "0.5.0";
+static const char *VERSION        = "0.5.1";
 static const char *HIDEMAINMENUEENTRY = "HideMainMenuEntry";
 static const char *MAINMENUENTRY  = trNOOP("Trayopenng");
 static const char *DESCRIPTION    = trNOOP("Open and close tray of CDROM");
